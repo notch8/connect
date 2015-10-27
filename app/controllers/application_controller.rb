77febf043bc48362c469b4674d67efa66b987256
Vincent Colavin <vcolavin@gmail.com>
2015-10-27 14:34:24 -0700

    donor sign in will redirect to previous url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,41 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    # sorry.
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        request.path != "/donors/sign_in" &&
+        request.path != "/donors/sign_up" &&
+        request.path != "/donors/password/new" &&
+        request.path != "/donors/password/edit" &&
+        request.path != "/donors/confirmation" &&
+        request.path != "/donors/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
   def after_sign_in_path_for(resource)
-    root_path
+    if current_user && current_user.needs.empty?
+      new_need_path
+    elsif current_user
+      need_path(current_user.needs.first)
+    elsif current_donor
+      # we need to take the donor back to the need they were looking at
+      session[:previous_url]
+    else
+      root_path
+    end
   end
 
   def current_organization
