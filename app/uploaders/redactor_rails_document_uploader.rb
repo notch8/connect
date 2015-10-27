0cb83f476ea5c3ba742704d7d59bbce7f3f6ff8a
Vincent Colavin <vcolavin@gmail.com>
2015-10-27 10:44:38 -0700

    WIP wysiwyg editor images save and show up properly

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -2,7 +2,7 @@
 class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
   include RedactorRails::Backend::CarrierWave
 
-  storage :fog
+  # storage :fog
   # storage :file
 
   def store_dir
