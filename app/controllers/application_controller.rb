89f21e728cecb3e8bd2b1434c86c6870b4d5bd19
Rob Kaufman <rob@notch8.com>
2015-10-27 21:51:23 -0700

    sign in / sign up flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,28 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
-
-  after_filter :store_location
+  before_filter :store_location
 
   def store_location
     # store last url - this is needed for post-login redirect to whatever the user last visited.
-    return unless request.get? 
+    return unless request.get?
     # sorry.
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        request.path != "/donors/sign_in" &&
-        request.path != "/donors/sign_up" &&
-        request.path != "/donors/password/new" &&
-        request.path != "/donors/password/edit" &&
-        request.path != "/donors/confirmation" &&
-        request.path != "/donors/sign_out" &&
+    if (!request.path.match(/^\/(users|donors)\/(sign|password|confirmation)/) &&
         !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath 
+      session[:previous_url] = request.fullpath
     end
   end
 
