d4f5767dbd5d9f9b37311b86e3419bd7dc7e0ddc
Rob Kaufman <rob@notch8.com>
2016-02-03 11:55:09 -0800

    fix posted_at missing validation

--- app/models/need.rb
+++ app/models/need.rb
@@ -8,6 +8,13 @@ class Need < ActiveRecord::Base
   friendly_id :title, use: :slugged
 
   validates :amount_requested, numericality: { greater_than: 0 }
+  validates :posted_at, presence: true
+
+  after_initialize :setup_posted_at
+
+  def setup_posted_at
+    self.posted_at = Time.now unless self.posted_at
+  end
 
   def percent_complete
     (self.amount_donated / self.amount_requested.to_f) * 100
