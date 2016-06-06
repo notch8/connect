16e060cf582f6ac89a2d3e44875e81bf9016821b
Rob Kaufman <rob@notch8.com>
2016-06-06 13:17:32 -0700

    fixes adding users via admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ActiveRecord::Base
 
 
   belongs_to :organization
-  attr_accessor :access_code
 
   has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "missing/:style.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
